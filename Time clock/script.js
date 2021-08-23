325983c1c1fb20710611285cd0a62898ede024c5
Smagulov Dastan <smagulovdastan07@gmail.com>
2021-08-23 12:22:13 +0600

    Initial commit

---
+++ Time clock/script.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hour = now.getHours();
+
+    const secondDegrees = ((seconds / 60) * 360 + 90);
+    const minuteDegrees = ((minutes/ 60) * 6 + 90);
+    const hourDegrees = ((hour/ 12) * 360 + 90);
+
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    minHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    hourHand.style.tranform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
